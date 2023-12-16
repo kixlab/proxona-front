7d80d03b5ad5308fde9523d8be6171c6eb28fceb
yoon0u0 <yoon0u0@gmail.com>
2023-12-16 20:58:37 +0900

    card component

---
+++ proxona-interface/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ProxonaProfile from './components/ProxonaProfile/ProxonaProfile.jsx';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ProxonaProfile />
+    </div>
+  );
+}
+
+export default App;

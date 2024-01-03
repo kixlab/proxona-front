056abba534a97105fb6980478371b25adb649d42
eunkang <ek646@cornell.edu>
2024-01-03 22:11:48 +0900

    end to end

--- proxona-interface/src/components/DiscoverProxonaModal/DiscoverProxona.jsx
+++ proxona-interface/src/components/DiscoverProxonaModal/DiscoverProxona.jsx
@@ -5,6 +5,7 @@ import "./DiscoverProxona.css";
 import { useNavigate, useLocation } from "react-router-dom";
 import { disableBodyScroll, enableBodyScroll } from "body-scroll-lock";
 import { ModalWrapper } from "../../pages/styles/DesignSystem";
+import SelectPersona from "../../pages/SelectPersona";
 
 const DiscoverProxona = () => {
 	const navigate = useNavigate();
@@ -44,34 +45,8 @@ const DiscoverProxona = () => {
 	return (
 		<ModalWrapper>
 			<div className="container detailproxona_container">
-				<button
-					className="btn"
-					ref={modalRef}
-					onClick={() => navigate(location.state.previousLocation.pathname)}
-				>
-					<i class="bi bi-x-lg"></i>
-				</button>
-				<p>
-					내 채널의 시청자들은 어떠한 다양한 면을 가지고 있을까요? <br></br>
-					시청자의 특성 3개 이상을 고르면, 그 특성을 반영한 시청자가
-					만들어집니다.{" "}
-				</p>
-				{Object.keys(features).map((feature, key) => {
-					return (
-						<div
-							className="d-flex flex-row justify-content-center align-items-center p-2"
-							key={key}
-						>
-							<div>{feature}</div>
-							{features[feature].map((element) => {
-								return (
-									<button className="btn btn-secondary m-2">{element}</button>
-								);
-							})}
-						</div>
-					);
-				})}
-				<button className="btn btn-secondary">시청자 페르소나 만들기</button>
+				<SelectPersona />
+				<button>시청자 페르소나 만들기</button>
 			</div>
 		</ModalWrapper>
 	);
--- proxona-interface/src/pages/SelectPersona.jsx
+++ proxona-interface/src/pages/SelectPersona.jsx
@@ -3,105 +3,10 @@ import { features2 } from "../data/dummy";
 import { Stack, Button, Box } from "@mui/material";
 import { Link, Route, Routes, useNavigate, useParams } from "react-router-dom";
 import "./styles/index.css";
-
-function groupBy(array, key) {
-	return array.reduce((result, currentItem) => {
-		// Get the value of the key for the current item
-		const keyValue = currentItem[key];
-
-		// If the key doesn't exist in the result object, initialize it with an empty array
-		if (!result[keyValue]) {
-			result[keyValue] = [];
-		}
-
-		// Add the current item to the array for the key
-		result[keyValue].push(currentItem);
-
-		return result;
-	}, {}); // Initialize the result as an empty object
-}
+import SelectAttributes from "../components/SelectAttributes/SelectAttributes";
 
 const SelectPersona = () => {
-	const [personaEle, setPersonaEle] = useState(features2);
-	const [selectedEle, setSelectedEle] = useState(features2);
-	const [submittedEles, setSubmittedEles] = useState(null);
-	const navigate = useNavigate();
-	const { id } = useParams();
-
-	const clickEle = useCallback((feature, element) => {
-		// setSelectedEle(element);
-
-		const result = groupBy(selectedEle, feature);
-		setSelectedEle(result[element]);
-	}, []);
-
-	const mergedFeature = useMemo(() => {
-		const mergedFeatures = personaEle.reduce((acc, curr) => {
-			Object.entries(curr).forEach(([key, value]) => {
-				if (!acc[key]) {
-					acc[key] = [];
-				}
-				if (!acc[key].includes(value)) {
-					acc[key].push(value);
-				}
-			});
-			return acc;
-		}, {});
-		return mergedFeatures;
-	}, [personaEle]);
-
-	const mergedSelectFeature = useMemo(() => {
-		const mergedSelectFeatures = selectedEle.reduce((acc, curr) => {
-			Object.entries(curr).forEach(([key, value]) => {
-				if (!acc[key]) {
-					acc[key] = [];
-				}
-				if (!acc[key].includes(value)) {
-					acc[key].push(value);
-				}
-			});
-			return acc;
-		}, {});
-		return Object.values(mergedSelectFeatures).flat(1);
-	}, [selectedEle]);
-
-	useEffect(() => {
-		setSubmittedEles(mergedFeature);
-	}, [selectedEle]);
-
-	return (
-		<Stack>
-			<Box> 내 채널의 시청자들은 어떠한 다양한 면을 가지고 있을까요? </Box>
-
-			{Object.keys(mergedFeature).map((feature, key) => {
-				return (
-					<div key={key}>
-						<div>{feature}</div>
-						{mergedFeature[feature].map((element) => {
-							const isDisabled = mergedSelectFeature.includes(element)
-								? ""
-								: "disabled";
-							return (
-								<Button
-									variant="contained"
-									onClick={() => clickEle(feature, element)}
-									disabled={isDisabled}
-								>
-									{element}
-								</Button>
-							);
-						})}
-					</div>
-				);
-			})}
-			{/* {submittedEles.map((ele) => {
-				return <div> {ele}</div>;
-			})} */}
-			<Button LinkComponent={Link} to={`/${id}/result`}>
-				다음
-			</Button>
-		</Stack>
-	);
+	return <SelectAttributes></SelectAttributes>;
 };
 
 export default SelectPersona;

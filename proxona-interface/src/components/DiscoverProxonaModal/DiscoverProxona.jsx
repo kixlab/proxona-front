5c20d28e66ca0b670457cd0ef69d99f4564b1a89
yoon0u0 <yoon0u0@gmail.com>
2024-01-11 04:37:39 +0900

    fix

--- proxona-interface/src/components/DiscoverProxonaModal/DiscoverProxona.jsx
+++ proxona-interface/src/components/DiscoverProxonaModal/DiscoverProxona.jsx
@@ -29,8 +29,8 @@ const DiscoverProxona = () => {
 		const dimval = {"성격특성" : "무심한", "구매특성" : "신중한"}
 		await axios
 			.post(
-				port + `youtube_api/${String(dimval)}/create-persona-exp/`,
-				JSON.stringify(selectedDimensionTrue),
+				port + `youtube_api/${JSON.stringify(dimval)}/create-persona-exp/`,
+				// JSON.stringify(selectedDimensionTrue),
 				{
 					headers: {
 						"Content-Type": "application/json",
